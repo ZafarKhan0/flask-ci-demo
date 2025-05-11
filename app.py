407f5a5591e934815831fc48f9f426171eaa2a06
ZafarKhan0 <bhuttozafarali195@gmail.com>
2025-05-11 10:54:21 +0500

    Fix flake8 formatting errors

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
+
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def home():
     return "Hello, CI/CD!"
 
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+# Add this newline at the end (no text after it!)
